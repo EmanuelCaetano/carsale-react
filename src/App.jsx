e57c20d268d3515e08c3a4be6ecbb44bf828f18b
Emanuel Caetano <emanuel.caetano.92@outlook.com>
2023-02-06 23:51:26 -0300

    create component and function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import "./global.css"
+import { footer } from "./componentes/footer";
+import { mainbody } from "./componentes/mainbody";
+import { menu } from "./componentes/menu";
+import { topbanner } from "././componentes/topbanner";
+import { topbar } from "./componentes/topbar";
+import { Routes, Route} from "react-router-dom";
+
+
 
 export function App() {
   return (
-  
-    <p>text</p>
-  )
+    <>
+    <topbar />
+    <topbanner />
+      <mainbody>
+        <menu />
+        <Routes>
+        <Route path="" element={home /} />
+
+        </Routes>
+      </mainbody>
+
+      <footer />
+      </>
+  );
+   
 }
 
 
